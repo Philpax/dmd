44afb6bf4124240ce5c955798c61fb5410a8f822
Philpax <me@philpax.me>
2017-02-21 23:19:29 +1100

    Lua/Test: Use each instead of foreach for ranges

--- test/lua/testRanges.d
+++ test/lua/testRanges.d
@@ -1,7 +1,6 @@
 void main() @safe
 {
     import std.range : iota;
-    import std.algorithm : map, filter;
-    foreach (i; 10.iota.map!(a => a * a).filter!(a => a > 50))
-        print(i);
+    import std.algorithm : map, filter, each;
+    10.iota.map!(a => a * a).filter!(a => a > 50).each!print;
 }
